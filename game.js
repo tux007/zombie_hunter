d165debb8457389cb2a6bf9b40352251f647e28e
Thomas Steimann <thomas@steimann.ch>
2025-10-16 20:33:00 +0200

    Slashing hinzugefügt

--- game.js
+++ game.js
@@ -3,25 +3,48 @@ addMap("./img/dead _forest.png");
 let character = new GameObject(
   "./img/crusader/Idle/0_Skeleton_Crusader_Idle_000.png",
   200,
-  700,
-  300
+  750,
+  200
 );
 character.animate(0, 17, 50);
+character.collisionOffsetX = 40;
+//character.showBorder();
+
+character.isColliding(function(zombie) {
+    if (character.isAttacking) {
+      zombie.flyAway(15, 10, 15);
+    }
+});
+
+spawnZombie();
+function spawnZombie() {
+  let zombie = new GameObject("./img/Zombie/Walk.png");
+  zombie.height = 300;
+  zombie.direction = -1;
+  zombie.x = 1000;
+  zombie.y = 615;
+  zombie.collisionOffsetX = 100;
+  //zombie.showBorder();
+  zombie.runLeft(3);
+  zombie.animateSpriteSheet(10, 100);
+
+  setTimeout(spawnZombie, Math.random() * 5000);
+}
 
 function spaceKeyPressed() {
   if (character.isAttacking) {
     return;
   }
-    character.isAttacking = true;
-    character.animateOnce(
+  character.isAttacking = true;
+  character.animateOnce(
     "./img/crusader/Slashing/0_Skeleton_Crusader_Slashing_000.png",
     0,
     11,
     30
   );
-    setTimeout(function() {
-        character.isAttacking = false;
-    }, 11 * 20);
+  setTimeout(function () {
+    character.isAttacking = false;
+  }, 11 * 20);
 }
 
 function rightKeyPressed() {
